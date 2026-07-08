84cc60244e0196206a02eb606dc7a44d61e1218f
yaakovizel <z7685727@gmail.com>
2026-07-08 07:22:17 +0300

    added try catch in server

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const server = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json");
   const baseUrl = `http://${req.headers.host}`;
   const parsedUrl = new URL(req.url, baseUrl);
-  if (parsedUrl.pathname.startsWith("/heroes")) {
-    await handleRoutes(req, res, parsedUrl);
-  } else {
-    res.writeHead(404);
-    res.end(JSON.stringify({ Message: "API route not found" }));
+  try {
+    if (parsedUrl.pathname.startsWith("/heroes")) {
+      await handleRoutes(req, res, parsedUrl);
+    } else {
+      res.writeHead(404);
+      res.end(JSON.stringify({ Message: "API route not found" }));
+    }
+  } catch (err) {
+    res.writeHead(500);
+    res.end(JSON.stringify({ Message: "Internal server error" }));
+    console.log(err.message);
   }
 });
 
